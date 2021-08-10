fc6dce9c900026c54f70c30035ca7b53b78eb5cb
Diego Gutierrez <diego_ggv@pm.me>
2021-08-10 08:43:29 -1000

    add scss folder

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+const search = new Filter('search', 'data-caption');
+
+baguetteBox.run('.gallery');
+
+
+
+// <script>
+//     window.addEventListener('load', function() {
+//     baguetteBox.run('.gallery');
+// });
+// </script>
