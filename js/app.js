77341e92ae03bab17be128e9c04e3aee8c6d1640
Diego Gutierrez <diego_ggv@pm.me>
2021-08-18 12:48:32 -1000

    add format/style to the gallery, and the searchbar.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,4 @@
 
-const search = new Filter('search', 'data-caption');
-
 baguetteBox.run('.gallery');
 
-
-
-// <script>
-//     window.addEventListener('load', function() {
-//     baguetteBox.run('.gallery');
-// });
-// </script>
+const search = new Filter('search', 'data-caption');
